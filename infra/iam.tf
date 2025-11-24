99d449b2b9b7453b43eb51fd0a2d30c1bed05a5a
AleHerreraSoria <a.n.h.soria@gmail.com>
2025-11-24 01:16:13 -0300

    Feat: Agrega infraestructura automatizada con Terraform (EC2 Spark + S3 Data Lake)

---
+++ infra/iam.tf
@@ -0,0 +1,27 @@
+# 1. Creación del Rol de IAM para la EC2 (CORREGIDO: me faltó el "_iam_role" en linea 2)
+resource "aws_iam_role" "spark_processor_role" {
+  name = "henry_spark_processor_role_dev"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# 2. Dar permisos FULL sobre S3 (Para leer Bronze y escribir Silver)
+resource "aws_iam_role_policy_attachment" "s3_access" {
+  role       = aws_iam_role.spark_processor_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+# 3. Creación del "Perfil de Instancia" (El enchufe para la EC2)
+resource "aws_iam_instance_profile" "spark_profile" {
+  name = "henry_spark_profile_dev"
+  role = aws_iam_role.spark_processor_role.name
+}
