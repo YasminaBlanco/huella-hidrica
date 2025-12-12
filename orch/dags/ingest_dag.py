8d23e8fe4696dc6edf5de5cf7750093f6653074e
jjdunbar1211 <csamignone@gmail.com>
2025-12-12 01:38:29 -0300

    orch: etl dag ok

--- orch/dags/ingest_dag.py
+++ orch/dags/ingest_dag.py
@@ -1,22 +1,23 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator, ShortCircuitOperator
-from airflow.operators.email import EmailOperator
-from airflow.providers.http.hooks.http import HttpHook
-from airflow.providers.ssh.operators.ssh import SSHOperator
-from airflow.providers.ssh.hooks.ssh import SSHHook
-from airflow.utils.task_group import TaskGroup
-from airflow.utils.dates import days_ago
-from airflow.models import Variable
-from datetime import datetime, timedelta, timezone
 import json
 import logging
 import os
+from datetime import datetime, timedelta, timezone
+
 import boto3
-import subprocess
+from airflow.models import Variable
+from airflow.operators.email import EmailOperator
+from airflow.operators.python import PythonOperator, ShortCircuitOperator
+from airflow.providers.http.hooks.http import HttpHook
+from airflow.providers.ssh.hooks.ssh import SSHHook
+from airflow.providers.ssh.operators.ssh import SSHOperator
+from airflow.utils.dates import days_ago
+from airflow.utils.task_group import TaskGroup
+
+from airflow import DAG
 
 # ----------------- Config -----------------
 API_CONN_ID = "agua_latam_api"
-LOG_FILE = "/opt/airflow/logs/main_ingest.json"   
+LOG_FILE = "/opt/airflow/logs/main_ingest.json"
 PYSPARK_INSTANCE_ID = "i-02024816014b00588"
 PYSPARK_PRIVATE_IP = "172.31.11.188"
 AWS_REGION = "us-east-2"
@@ -34,9 +35,10 @@ logger = logging.getLogger("main_ingest_logger")
 logger.setLevel(logging.INFO)
 if not logger.handlers:
     handler = logging.FileHandler(LOG_FILE)
-    formatter = logging.Formatter('%(message)s')
+    formatter = logging.Formatter("%(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
+
 
 def log_result(task_id, endpoint, status, message=""):
     entry = {
@@ -59,7 +61,9 @@ def log_result(task_id, endpoint, status, message=""):
         json.dump(data, f, indent=2)
     logger.info(json.dumps(entry))
 
+
 # ----------------- Funciones Ingesta -----------------
+
 
 # healthcheck de la ingest_api
 def ingest_api_healthcheck(task_id):
@@ -76,6 +80,7 @@ def ingest_api_healthcheck(task_id):
     except Exception as e:
         log_result(task_id, "/health", "FAILURE", str(e))
         raise
+
 
 # consumo de la ingest_api
 def call_endpoint_with_logging(task_id, endpoint, params=None):
@@ -95,10 +100,11 @@ def call_endpoint_with_logging(task_id, endpoint, params=None):
 
 
 def run_once_check(task_id):
-    if Variable.get(task_id, default_var=None):   # flag ya existe
+    if Variable.get(task_id, default_var=None):  # flag ya existe
         return False
-    Variable.set(task_id, "done")                 # setea flag
+    Variable.set(task_id, "done")  # setea flag
     return True
+
 
 def should_run_annual():
     today = datetime.now(timezone.utc)
@@ -107,6 +113,7 @@ def should_run_annual():
     if last_run_year is not None and int(last_run_year) == current_year:
         return False
     return True
+
 
 def mark_annual_done():
     today = datetime.now(timezone.utc)
@@ -118,15 +125,17 @@ def mark_annual_done():
 def start_ec2():
     ec2 = boto3.client("ec2", region_name=AWS_REGION)
     ec2.start_instances(InstanceIds=[PYSPARK_INSTANCE_ID])
-    waiter = ec2.get_waiter('instance_running')
+    waiter = ec2.get_waiter("instance_running")
     waiter.wait(InstanceIds=[PYSPARK_INSTANCE_ID])
     print("EC2 PySpark lista y corriendo")
+
 
 # apagar ec2
 def stop_ec2():
     ec2 = boto3.client("ec2", region_name=AWS_REGION)
     ec2.stop_instances(InstanceIds=[PYSPARK_INSTANCE_ID])
     print("EC2 PySpark apagada")
+
 
 def can_run_transformations():
     """Decide si se ejecutan transformaciones según reglas"""
@@ -142,7 +151,8 @@ def can_run_transformations():
             return False
     return True
 
-#ejecutar job de pyspark
+
+# ejecutar job de pyspark
 def spark_cmd(job):
     today = datetime.now()
     year = today.year
@@ -170,7 +180,9 @@ def spark_cmd(job):
             needs_base = False
 
         case _:
-            raise ValueError("Job inválido. Opciones: silver | silver_model | gold_model | test")
+            raise ValueError(
+                "Job inválido. Opciones: silver | silver_model | gold_model | test"
+            )
 
     base = "cd /opt/elt/app && \\"
 
@@ -194,6 +206,7 @@ def spark_cmd(job):
         '
     """
 
+
 # ----------------- DAG -----------------
 default_args = {
     "owner": "pf-shde",
@@ -207,26 +220,35 @@ with DAG(
     schedule_interval="0 6 * * *",
     catchup=False,
     default_args=default_args,
-    tags=["ingestion","transform"],
+    tags=["ingestion", "transform"],
 ) as dag:
 
     # ingesta JMP
     run_unique_ingest = ShortCircuitOperator(
         task_id="run_unique_ingest",
-        python_callable=lambda: run_once_check(UNIQUE_INGEST_FLAG)
+        python_callable=lambda: run_once_check(UNIQUE_INGEST_FLAG),
     )
 
     health_unique = PythonOperator(
         task_id="health_unique",
-        python_callable=lambda: ingest_api_healthcheck("health_unique")
+        python_callable=lambda: ingest_api_healthcheck("health_unique"),
     )
 
     task_unique_ingest = PythonOperator(
         task_id="ingest_unique",
-        python_callable=lambda: call_endpoint_with_logging("ingest_unique", "/ingest/world-bank",
-                                                          {"countries":"ARG","indicators":"NY.GDP","start_year":2023,"end_year":2024,"isTest":True}),
+        python_callable=lambda: call_endpoint_with_logging(
+            "ingest_unique",
+            "/ingest/world-bank",
+            {
+                "countries": "ARG",
+                "indicators": "NY.GDP",
+                "start_year": 2023,
+                "end_year": 2024,
+                "isTest": True,
+            },
+        ),
         retries=3,
-        retry_delay=timedelta(minutes=5)
+        retry_delay=timedelta(minutes=5),
     )
 
     notify_unique_success = EmailOperator(
@@ -243,26 +265,33 @@ with DAG(
 
     # ingesta wb
     run_annual_ingest = ShortCircuitOperator(
-        task_id="run_annual_ingest",
-        python_callable=should_run_annual
+        task_id="run_annual_ingest", python_callable=should_run_annual
     )
 
     health_annual = PythonOperator(
         task_id="health_annual",
-        python_callable=lambda: ingest_api_healthcheck("health_annual")
+        python_callable=lambda: ingest_api_healthcheck("health_annual"),
     )
 
     task_annual_ingest = PythonOperator(
         task_id="ingest_annual",
-        python_callable=lambda: call_endpoint_with_logging("ingest_unique", "/ingest/world-bank",
-                                                          {"countries":"ARG","indicators":"NY.GDP","start_year":2023,"end_year":2024,"isTest":True}),
+        python_callable=lambda: call_endpoint_with_logging(
+            "ingest_unique",
+            "/ingest/world-bank",
+            {
+                "countries": "ARG",
+                "indicators": "NY.GDP",
+                "start_year": 2023,
+                "end_year": 2024,
+                "isTest": True,
+            },
+        ),
         retries=3,
-        retry_delay=timedelta(minutes=5)
+        retry_delay=timedelta(minutes=5),
     )
 
     mark_annual = PythonOperator(
-        task_id="mark_annual",
-        python_callable=mark_annual_done
+        task_id="mark_annual", python_callable=mark_annual_done
     )
 
     notify_annual_success = EmailOperator(
@@ -274,31 +303,45 @@ with DAG(
         conn_id="mailgun_smtp",
     )
 
-    # run_annual_ingest >> health_annual >> task_annual_ingest >> mark_annual 
-    run_annual_ingest >> health_annual >> task_annual_ingest >> [mark_annual, notify_annual_success]
+    # run_annual_ingest >> health_annual >> task_annual_ingest >> mark_annual
+    (
+        run_annual_ingest
+        >> health_annual
+        >> task_annual_ingest
+        >> [mark_annual, notify_annual_success]
+    )
 
     # open meteo
     check_weekly_ingest = ShortCircuitOperator(
         task_id="check_weekly_ingest",
-        python_callable=lambda: Variable.get(WEEKLY_INGEST_FLAG, default_var=None) is None
+        python_callable=lambda: Variable.get(WEEKLY_INGEST_FLAG, default_var=None)
+        is None,
     )
 
     health_weekly = PythonOperator(
         task_id="health_weekly",
-        python_callable=lambda: ingest_api_healthcheck("health_weekly")
+        python_callable=lambda: ingest_api_healthcheck("health_weekly"),
     )
 
     task_weekly_ingest = PythonOperator(
         task_id="ingest_weekly",
-        python_callable=lambda: call_endpoint_with_logging("ingest_weekly", "/ingest/open-meteo",
-                                                          {"country":"ARG","start_date":"2023-10-01","end_date":"2024-10-01","isTest":True}),
+        python_callable=lambda: call_endpoint_with_logging(
+            "ingest_weekly",
+            "/ingest/open-meteo",
+            {
+                "country": "ARG",
+                "start_date": "2023-10-01",
+                "end_date": "2024-10-01",
+                "isTest": True,
+            },
+        ),
         retries=3,
-        retry_delay=timedelta(minutes=5)
+        retry_delay=timedelta(minutes=5),
     )
 
     mark_weekly_done = PythonOperator(
         task_id="mark_weekly_done",
-        python_callable=lambda: Variable.set(WEEKLY_INGEST_FLAG,"done")
+        python_callable=lambda: Variable.set(WEEKLY_INGEST_FLAG, "done"),
     )
 
     notify_weekly_success = EmailOperator(
@@ -310,27 +353,29 @@ with DAG(
         conn_id="mailgun_smtp",
     )
 
-    # check_weekly_ingest >> health_weekly >> task_weekly_ingest >> mark_weekly_done 
-    check_weekly_ingest >> health_weekly >> task_weekly_ingest >> [mark_weekly_done, notify_weekly_success]
+    # check_weekly_ingest >> health_weekly >> task_weekly_ingest >> mark_weekly_done
+    (
+        check_weekly_ingest
+        >> health_weekly
+        >> task_weekly_ingest
+        >> [mark_weekly_done, notify_weekly_success]
+    )
 
     # T
 
     transform_gate = ShortCircuitOperator(
         task_id="transform_gate",
-        python_callable=lambda: True
+        python_callable=lambda: True,
         # python_callable=can_run_transformations
     )
 
     start_ec2_task = PythonOperator(
         task_id="start_ec2",
-        python_callable=lambda: True
+        python_callable=lambda: True,
         # python_callable=start_ec2
     )
 
-    ssh_hook = SSHHook(
-    ssh_conn_id="ssh_spark_ec2",
-    timeout=3600  
-    )
+    ssh_hook = SSHHook(ssh_conn_id="ssh_spark_ec2", timeout=3600)
 
     with TaskGroup("spark_jobs_group") as spark_jobs:
 
@@ -339,7 +384,7 @@ with DAG(
             ssh_hook=ssh_hook,
             command=spark_cmd("test"),
             get_pty=True,
-            cmd_timeout=60 * 60 * 3
+            cmd_timeout=60 * 60 * 3,
         )
 
         run_silver_task = SSHOperator(
@@ -348,7 +393,7 @@ with DAG(
             # ssh_conn_id="ssh_spark_ec2",
             command=spark_cmd("silver"),
             get_pty=True,
-            cmd_timeout=60 * 60 * 3
+            cmd_timeout=60 * 60 * 3,
         )
 
         run_silver_model_task = SSHOperator(
@@ -357,7 +402,7 @@ with DAG(
             # ssh_conn_id="ssh_spark_ec2",
             command=spark_cmd("silver_model"),
             get_pty=True,
-            cmd_timeout=60 * 60 * 3
+            cmd_timeout=60 * 60 * 3,
         )
 
         run_gold_task = SSHOperator(
@@ -366,7 +411,7 @@ with DAG(
             # ssh_conn_id="ssh_spark_ec2",
             command=spark_cmd("gold_model"),
             get_pty=True,
-            cmd_timeout=60 * 60 * 3
+            cmd_timeout=60 * 60 * 3,
         )
 
         run_test_task >> run_silver_task >> run_silver_model_task >> run_gold_task
@@ -374,7 +419,7 @@ with DAG(
     stop_ec2_task = PythonOperator(
         task_id="stop_ec2",
         # python_callable=stop_ec2
-        python_callable=lambda: True
+        python_callable=lambda: True,
     )
 
     notify_transform_success = EmailOperator(
@@ -389,4 +434,3 @@ with DAG(
     [task_unique_ingest, task_annual_ingest, task_weekly_ingest] >> transform_gate
     transform_gate >> start_ec2_task >> run_test_task
     run_gold_task >> [stop_ec2_task, notify_transform_success]
-    # transform_gate >> start_ec2_task >> run_silver_task >> run_silver_model_task >> run_gold_task >> notify_transform_success >> stop_ec2_task
